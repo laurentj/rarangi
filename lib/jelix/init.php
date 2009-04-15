cac4d5729d569d815935cd225428f84e7cf2d0c1
laurentj <unknown>
2009-04-15 08:05:22 +0000

    Updated jelix to its latest trunk version

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -17,7 +17,7 @@
  * Version number of Jelix
  * @name  JELIX_VERSION
  */
-define ('JELIX_VERSION', '1.2b1pre.1372');
+define ('JELIX_VERSION', '1.2b1pre.1391');
 
 /**
  * base of namespace path used in xml files of jelix
