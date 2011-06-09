59f189b09fa206f553f2becc0f167f2679d3d30c
Laurent Jouanneau <laurent@xulfr.org>
2011-06-09 09:17:45 +0200

    jelix updrade

--- lib/installwizard/jtpl/jTpl.class.php
+++ lib/installwizard/jtpl/jTpl.class.php
@@ -16,7 +16,7 @@
  */
 class jTpl {
 
-    const VERSION = '1.0pre.1958';
+    const VERSION = '1.0pre.1962';
 
     /**
      * all assigned template variables. 
--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -17,7 +17,7 @@
  * Version number of Jelix
  * @name  JELIX_VERSION
  */
-define ('JELIX_VERSION', '1.3b2pre.1958');
+define ('JELIX_VERSION', '1.3b2pre.1962');
 
 /**
  * base of namespace path used in xml files of jelix
